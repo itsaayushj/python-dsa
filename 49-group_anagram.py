9538236feb7ef44d6ea93c500af0bb1b95cac2e7
Aayush jha <a2jha1909@gmail.com>
2026-08-15 20:46:46 +0530

    sovled group anagram

--- 49-group_anagram.py
+++ 49-group_anagram.py
@@ -3,15 +3,30 @@
 # from collections import defaultdict
 # res = defaultdict(list)
 
-from collections import defaultdict
+# from collections import defaultdict
+# strs = ["eat","tea","tan","ate","nat","bat"]
+# hashmap = defaultdict(list)
+
+# for str in strs :
+#     count = [0] * 26
+#     for i in str : 
+#         count[ord(i) - ord("a")] += 1 
+
+#     key = tuple(count)
+#     hashmap[key].append(str)
+# print(hashmap.values())
+
 strs = ["eat","tea","tan","ate","nat","bat"]
+from collections import defaultdict
 hashmap = defaultdict(list)
-
-for str in strs :
-    count = [0] * 26
+for str in strs : 
+    count_key = [0] * 26 # 26 as in 26 words in alphabet , we will do +1 on 0 if the word is inside str...it will be used as key later in dictionary 
     for i in str : 
-        count[ord(i) - ord("a")] += 1 
+        count_key[ord(i) - ord("a")] += 1 
+    count_key = tuple(count_key)
+    hashmap[count_key].append(str)
 
-    key = tuple(count)
-    hashmap[key].append(str)
 print(hashmap.values())
+
+
+
